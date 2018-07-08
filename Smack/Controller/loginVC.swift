3831d960537f3b64134e059129ef2214d94a7326
Jonah Travis <master@Jonahs-iMac.fios-router.home>
2018-07-07 20:29:12 -0400

    Lesson-04

---
+++ Smack/Controller/loginVC.swift
@@ -0,0 +1,25 @@
+//
+//  loginVC.swift
+//  Smack
+//
+//  Created by Jonah Travis on 7/7/18.
+//  Copyright © 2018 Jonah Travis. All rights reserved.
+//
+
+import UIKit
+
+class loginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cloePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Jonah Travis on 7/7/18.
+//  Copyright © 2018 Jonah Travis. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
